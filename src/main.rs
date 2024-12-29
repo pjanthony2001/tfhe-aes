c41a75987cc796e4e7b65172665b194777ef0f02
Apzure <nigelleexy@gmail.com>
2024-12-29 16:00:28 +0800

    Implement Aes Clear encryption for CBC, CTR and ECB

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use hex;
+use aes::cipher::{block_padding::Pkcs7, BlockEncryptMut, KeyIvInit, KeyInit, StreamCipher, generic_array::GenericArray, BlockEncrypt};
+use aes::Aes128;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -19,8 +22,17 @@ struct Args {
     mode: String,
 }
 
+enum Mode {
+    ECB,
+    CBC,
+    CTR,
+}
+
+const BLOCK_SIZE_IN_BYTES: usize = 16;
+
+
 fn main() {
-    // Example usage: .\clapper.exe -n 2 --iv 111111111111111111111111111111 --key 123 --key-expansion-offline --mode transform  
+    // Example usage: .\aes_clear.exe --number-of-outputs 11 --iv 11111111111111111111111111111111 --key AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  --mode ECB
 
     let args = Args::parse();
 
@@ -29,4 +41,80 @@ fn main() {
     println!("Key: {}", args.key);
     println!("Key Expansion Offline: {}", args.key_expansion_offline);
     println!("Mode: {}", args.mode);
+
+    let key = parse_hex_16(&args.key).expect("Invalid key format");
+    let iv = parse_hex_16(&args.iv).expect("Invalid IV format");
+    println!("Parsed Key: {:?}", key);
+    println!("Parsed IV: {:?}", iv);
+
+    let plaintext = *b"hello world! this is my plaintext.";
+    let mode = parse_mode(&args.mode);
+    let ciphertext = aes_clear_encrypt(&plaintext, key, iv, mode);
+    println!("Ciphertext: {:?}", ciphertext);
+}
+
+fn parse_mode(input: &str) -> Result<Mode, String> {
+    match input {
+        "ECB" => Ok(Mode::ECB),
+        "CBC" => Ok(Mode::CBC),
+        "CTR" => Ok(Mode::CTR),
+        _ => Err(format!("Invalid mode: {}", input)),
+    }
+}
+
+fn parse_hex_16(hex_str: &str) -> Result<[u8; 16], String> {
+    if hex_str.len() != 32 {
+        return Err(format!("Must be 32 hex characters (16 bytes), it is currently {} characters.", hex_str.len()));
+    }
+    let bytes = hex::decode(hex_str).map_err(|_| "Failed to decode hex")?;
+    let mut array = [0u8; 16];
+    array.copy_from_slice(&bytes[..16]);
+    Ok(array)
+}
+
+fn aes_clear_encrypt(plaintext: &[u8], key: [u8; 16], iv: [u8; 16], mode: Result<Mode, String>) -> Vec<u8> {
+    match mode {
+        Ok(Mode::CBC) => aes_clear_encrypt_cbc(&plaintext, key, iv),
+        Ok(Mode::CTR) => aes_clear_encrypt_ctr(&plaintext, key, iv),
+        Ok(Mode::ECB) => aes_clear_encrypt_ecb(&plaintext, key),
+        Err(e) => panic!("Failed to determine mode: {:?}", e),
+    }
+}
+
+fn aes_clear_encrypt_cbc(plaintext: &[u8], key: [u8; 16], iv: [u8; 16]) -> Vec<u8> {
+    type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
+    let pt_len = plaintext.len();
+    let buffer_size = ((pt_len + BLOCK_SIZE_IN_BYTES - 1) / BLOCK_SIZE_IN_BYTES) * BLOCK_SIZE_IN_BYTES;
+    let mut buf = vec![0u8; buffer_size];
+    buf[..plaintext.len()].copy_from_slice(plaintext);
+    let ct = Aes128CbcEnc::new(&key.into(), &iv.into())
+    .encrypt_padded_mut::<Pkcs7>(&mut buf, pt_len)
+    .unwrap();
+    ct.to_vec()
+}
+
+fn aes_clear_encrypt_ctr(plaintext: &[u8], key: [u8; 16], iv: [u8; 16]) -> Vec<u8> {
+    type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
+    let mut buf = plaintext.to_vec();
+    let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
+    cipher.apply_keystream(&mut buf);
+    return buf;
+}
+
+fn aes_clear_encrypt_ecb(plaintext: &[u8], key: [u8; 16]) -> Vec<u8> {
+    let cipher = Aes128::new(&GenericArray::from(key));
+
+    let pt_len = plaintext.len();
+    let padding_size = 16 - (pt_len % 16);
+    let mut padded_plaintext = Vec::from(plaintext);
+
+    padded_plaintext.extend(vec![0u8; padding_size]);
+
+    let mut ciphertext = Vec::with_capacity(padded_plaintext.len());
+    for chunk in padded_plaintext.chunks(16) {
+        let mut block = GenericArray::clone_from_slice(chunk); 
+        cipher.encrypt_block(&mut block);
+        ciphertext.extend_from_slice(&block);
+    }
+    ciphertext
 }
